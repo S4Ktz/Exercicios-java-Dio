7de8f2a5289684b471cc3597246cd501b4d1d1c8
S4Ktz <jaaow11@gmail.com>
2025-05-22 13:36:49 -0300

    Create exercicio1.java

---
+++ src/Exercicios/exercicio1.java
@@ -0,0 +1,17 @@
+package Exercicios;
+
+import java.time.OffsetDateTime;
+import java.util.Scanner;
+
+public class exercicio1 {
+    public static void main(String[] args) {
+        var BaseYear = OffsetDateTime.now().getYear();
+       var scanner = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        var nome = scanner.next();
+        System.out.println("Informe seu ano de nascimento: ");
+        var year = scanner.nextInt();
+        var age = BaseYear - year;
+        System.out.printf("Olá, %s você tem %s anos", nome , age);
+    }
+}
