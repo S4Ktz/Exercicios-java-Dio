a6d29ff674f0ac1040ecd7a792b4e4ed7cbdcb91
S4Ktz <jaaow11@gmail.com>
2025-06-01 20:00:31 -0300

    Update exercicio1.java

--- src/Exercicios/exercicio1.java
+++ src/Exercicios/exercicio1.java
@@ -1,17 +1,29 @@
 package Exercicios;
 
+import com.sun.source.tree.DoWhileLoopTree;
+
 import java.time.OffsetDateTime;
 import java.util.Scanner;
 
 public class exercicio1 {
     public static void main(String[] args) {
-        var BaseYear = OffsetDateTime.now().getYear();
-       var scanner = new Scanner(System.in);
-        System.out.println("Digite seu nome: ");
-        var nome = scanner.next();
-        System.out.println("Informe seu ano de nascimento: ");
-        var year = scanner.nextInt();
-        var age = BaseYear - year;
-        System.out.printf("Olá, %s você tem %s anos", nome , age);
+//        var BaseYear = OffsetDateTime.now().getYear();
+//       var scanner = new Scanner(System.in);
+//        System.out.println("Digite seu nome: ");
+//        var nome = scanner.next();
+//        System.out.println("Informe seu ano de nascimento: ");
+//        var year = scanner.nextInt();
+//        var age = BaseYear - year;
+//        System.out.printf("Olá, %s você tem %s anos" , nome , age);
+
+        int i = 0;
+      do {
+          System.out.println(i);
+          i++;
+      }
+      while(i < 4);
+
+
+
     }
 }
