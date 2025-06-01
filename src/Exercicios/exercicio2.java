b6c7e461268a7a7f9dccbfcdfa2d6af8b7a10ce6
S4Ktz <jaaow11@gmail.com>
2025-06-01 20:00:36 -0300

    Create exercicio2.java

---
+++ src/Exercicios/exercicio2.java
@@ -0,0 +1,12 @@
+package Exercicios;
+
+public class exercicio2 {
+    public static void main(String[] args) {
+
+        for (var i = 0; i < 5; i++) {
+
+          System.out.println(i + " - " + "teste");
+        }
+
+    }
+}
