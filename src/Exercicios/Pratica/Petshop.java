6cef4fb00445e7c883a8bdd6e7e0c89c3edc1f79
S4Ktz <jaaow11@gmail.com>
2025-06-03 07:15:09 -0300

    Update Petshop.java

--- src/Exercicios/Pratica/Petshop.java
+++ src/Exercicios/Pratica/Petshop.java
@@ -1,4 +1,29 @@
 package Exercicios.Pratica;
 
 public class Petshop {
+    int aguaL;
+    int shaampoL;
+    int quantidadePet;
+
+    public void maquinaDeBanho(){
+
+
+        if (quantidadePet < 1){
+            System.out.println("A maquina está livre ✔");
+        }else{
+            System.out.println("A maquina está ocupada❌\n" +
+                    "quantidade de pet: "+quantidadePet);
+        }
+
+
+    }
+
+    public void banho(){
+
+
+        var utilizarAgua  = aguaL -=10;
+        var utilizarShaampo = shaampoL -=2;
+        
+
+    }
 }
