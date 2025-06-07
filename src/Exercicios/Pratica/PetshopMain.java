b14a024b807b1a0c4e1db0e84f75bf5c77c19fa0
S4Ktz <jaaow11@gmail.com>
2025-06-07 19:41:47 -0300

    testando Petshop

---
+++ src/Exercicios/Pratica/PetshopMain.java
@@ -0,0 +1,14 @@
+package Exercicios.Pratica;
+
+public class PetshopMain {
+    public static void main(String[] args) {
+        Petshop petshop = new Petshop();
+        petshop.adicionarPet();
+        petshop.maquinaDeBanho();
+        petshop.PET();
+        petshop.reabastecerMaquina(2,2);
+        petshop.PetshopMenu();
+    }
+
+
+}
