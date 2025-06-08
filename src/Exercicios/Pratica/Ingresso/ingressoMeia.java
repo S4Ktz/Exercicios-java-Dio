62cd8e66d161066e5b9d67d8caecb1ea4046fb4f
S4Ktz <jaaow11@gmail.com>
2025-06-07 23:30:15 -0300

    Update ingressoMeia.java

--- src/Exercicios/Pratica/Ingresso/ingressoMeia.java
+++ src/Exercicios/Pratica/Ingresso/ingressoMeia.java
@@ -1,4 +1,9 @@
 package Exercicios.Pratica.Ingresso;
 
-public class ingressoMeia {
+public class ingressoMeia extends Ingresso{
+    public void IngressoMeiaEntrada(){
+      setValor(getValor()/2);
+        System.out.println("Você comprou um Ingresso de meia entrada");
+        this.setIngresso("Meia Entrada");
+    }
 }
