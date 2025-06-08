b1e4716bc63bf4d63f6ee4daf375147e5dbc6105
S4Ktz <jaaow11@gmail.com>
2025-06-07 23:30:12 -0300

    Create IngressoFamilia.java

---
+++ src/Exercicios/Pratica/Ingresso/IngressoFamilia.java
@@ -0,0 +1,22 @@
+package Exercicios.Pratica.Ingresso;
+
+public class IngressoFamilia extends Ingresso {
+    int membrosdafamilia;
+
+    public void AdicionarMembro(){
+        membrosdafamilia++;
+    }
+
+    public void IngressoFamiliar(){
+        if (membrosdafamilia <)
+    }
+
+
+    public int getMembrosdafamilia() {
+        return membrosdafamilia;
+    }
+
+    public void setMembrosdafamilia(int membrosdafamilia) {
+        this.membrosdafamilia = membrosdafamilia;
+    }
+}
