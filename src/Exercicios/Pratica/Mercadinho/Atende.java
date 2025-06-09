88279b1c9b1af8e9a4a04c19c26faec75f8388bb
S4Ktz <jaaow11@gmail.com>
2025-06-09 09:19:31 -0300

    Create Atende.java

---
+++ src/Exercicios/Pratica/Mercadinho/Atende.java
@@ -0,0 +1,52 @@
+package Exercicios.Pratica.Mercadinho;
+
+import java.util.Scanner;
+
+public final class Atende extends Conta {
+    Scanner scanner = new Scanner(System.in);
+    int ValorDoCaixa;
+
+    @Override
+    public void setAdministrador(boolean administrador) {
+        super.setAdministrador(administrador = false);
+    }
+
+    public Atende() {
+        System.out.println("Digite seu Usuario/nome: ");
+        Usuario = scanner.next();
+
+        System.out.println("Digite sua senha: ");
+        senha = scanner.nextInt();
+        if (senha == 12345) {
+            System.out.println("senha correta");
+        } else {
+            System.out.println("senha incorreta");
+
+        }
+
+
+        System.out.println("Digite seu email: ");
+        email = scanner.next();
+
+        if (this.getEmail().endsWith("@gmail.com")) {
+            System.out.println("Email logado com sucesso");
+        } else {
+            System.out.println("User@gmail invalido");
+        }
+
+        System.out.println(isAdministrador() ? "Conta Administrativa":"Conta Normal");
+
+        scanner.close();
+    }
+
+
+
+
+    public int getValorDoCaixa() {
+        return ValorDoCaixa;
+    }
+
+    public void setValorDoCaixa(int valorDoCaixa) {
+        ValorDoCaixa = valorDoCaixa;
+    }
+}
