58adbb35e9c2aa97b507171d5389d07721367db5
S4Ktz <jaaow11@gmail.com>
2025-06-09 08:31:06 -0300

    Update Vendedor.java

--- src/Exercicios/Pratica/Mercadinho/Vendedor.java
+++ src/Exercicios/Pratica/Mercadinho/Vendedor.java
@@ -1,4 +1,52 @@
 package Exercicios.Pratica.Mercadinho;
 
-public class Vendedor {
+import java.util.Scanner;
+
+public non-sealed class Vendedor extends Conta {
+
+int quantidadeDeVendas;
+
+    Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void setAdministrador(boolean administrador) {
+        super.setAdministrador(administrador = false);
+    }
+
+    public Vendedor() {
+
+        System.out.println("Digite seu Usuario/nome: ");
+        Usuario = scanner.next();
+
+        System.out.println("Digite sua senha: ");
+        senha = scanner.nextInt();
+        if (senha == 12345) {
+            System.out.println("senha correta");
+        } else {
+            System.out.println("senha incorreta");
+
+        }
+
+
+        System.out.println("Digite seu email: ");
+        email = scanner.next();
+
+        if (this.getEmail().endsWith("@gmail.com")) {
+            System.out.println("Email logado com sucesso");
+        } else {
+            System.out.println("User@gmail invalido");
+        }
+
+        System.out.println(isAdministrador() ? "Conta Administrativa":"Conta Normal");
+
+        scanner.close();
+    }
+
+    public int getQuantidadeDeVendas() {
+        return quantidadeDeVendas;
+    }
+
+    public void setQuantidadeDeVendas(int quantidadeDeVendas) {
+        this.quantidadeDeVendas = quantidadeDeVendas;
+    }
 }
