2b6f896aab762c3a5396477435ec96b1c514008d
S4Ktz <jaaow11@gmail.com>
2025-06-09 20:36:24 -0300

    Update MercadinhoMain.java

--- src/Exercicios/Pratica/Mercadinho/MercadinhoMain.java
+++ src/Exercicios/Pratica/Mercadinho/MercadinhoMain.java
@@ -1,8 +1,27 @@
 package Exercicios.Pratica.Mercadinho;
 
+import java.util.Scanner;
+
 public class MercadinhoMain {
+    Scanner scanner = new Scanner(System.in);
     Conta conta = new Conta();
     Vendedor vendedor = new Vendedor();
     Atendente atendente = new Atendente();
-    
+    Gerente gerente = new Gerente();
+
+
+
+   public void GerenteMenu(){
+       System.out.println("Nome/usuario: \n" +this.gerente.getUsuario() +
+                         "Email: \n" + "Tipo de conta: "+this.gerente.isAdministrador());
+       this.gerente.GerarRelatorio(vendedor.RelatorioDeVendas());
+   }
+
+
+
+
+
+
+
 }
+
