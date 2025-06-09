d2edbd0e1c72097505fad30307f96b36f2d4242b
S4Ktz <jaaow11@gmail.com>
2025-06-09 20:06:47 -0300

    Update MercadinhoMain.java

--- src/Exercicios/Pratica/Mercadinho/MercadinhoMain.java
+++ src/Exercicios/Pratica/Mercadinho/MercadinhoMain.java
@@ -1,4 +1,8 @@
 package Exercicios.Pratica.Mercadinho;
 
 public class MercadinhoMain {
+    Conta conta = new Conta();
+    Vendedor vendedor = new Vendedor();
+    Atendente atendente = new Atendente();
+    
 }
