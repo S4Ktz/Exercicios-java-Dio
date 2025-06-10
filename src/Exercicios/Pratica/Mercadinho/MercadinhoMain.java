59760d7d61ff09e3e8fa281b025d035b7ec9da61
S4Ktz <jaaow11@gmail.com>
2025-06-10 00:29:02 -0300

    Update MercadinhoMain.java

--- src/Exercicios/Pratica/Mercadinho/MercadinhoMain.java
+++ src/Exercicios/Pratica/Mercadinho/MercadinhoMain.java
@@ -2,9 +2,11 @@ package Exercicios.Pratica.Mercadinho;
 
 import java.util.Scanner;
 
-public class MercadinhoMain {
+public final class MercadinhoMain {
     private static Gerente gerente;
     private static Vendedor vendedor;
+    private static Atendente atendente;
+
     public void Contas() {
         Conta conta = new Conta();
         Vendedor vendedor = new Vendedor();
@@ -13,14 +15,28 @@ public class MercadinhoMain {
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Escolha uma opção");
         String escolha = scanner.next();
         switch (escolha){
-            case "1" -> Gerente.GerarRelatorio();
-            case "2" -> Vendedor.realizarVenda(10);
+            case "1","Relatorio" -> Gerente.GerarRelatorio();
+            case "2","Realizar Venda" -> Vendedor.realizarVenda();
+            case "3","Inserir Pagamento" -> {
+                System.out.println("Insira o Valor a ser pago: ");
+               int pagamento = Integer.parseInt(scanner.next());
+               if (atendente.Caixa){
+                   System.out.println("Caixa está aberto para pagamentos\n" +
+                           "pagamento realizado com sucesso\n" +
+                           "Valor do pagemento: "+pagamento);
+               }else{
+                   sout
+               }
+            }
         }
+
      Gerente.GerarRelatorio();
-     Vendedor.realizarVenda(10);
+     Vendedor.realizarVenda();
+
         System.out.println(Vendedor.RelatorioDeVendas());
     }
 
