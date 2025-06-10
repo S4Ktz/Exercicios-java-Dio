a30ab1f1267930ee67d3376ac9e165aebd60152e
S4Ktz <jaaow11@gmail.com>
2025-06-09 23:21:25 -0300

    Update MercadinhoMain.java

--- src/Exercicios/Pratica/Mercadinho/MercadinhoMain.java
+++ src/Exercicios/Pratica/Mercadinho/MercadinhoMain.java
@@ -3,18 +3,26 @@ package Exercicios.Pratica.Mercadinho;
 import java.util.Scanner;
 
 public class MercadinhoMain {
-    Scanner scanner = new Scanner(System.in);
-    Conta conta = new Conta();
-    Vendedor vendedor = new Vendedor();
-    Atendente atendente = new Atendente();
-    Gerente gerente = new Gerente();
-
-
+    private static Gerente gerente;
+    private static Vendedor vendedor;
+    public void Contas() {
+        Scanner scanner = new Scanner(System.in);
+        Conta conta = new Conta();
+        Vendedor vendedor = new Vendedor();
+        Atendente atendente = new Atendente();
+        Gerente gerente = new Gerente();
+    }
+    public static void main(String[] args) {
+     Gerente.GerarRelatorio();
+     Vendedor.realizarVenda(10);
+        System.out.println(Vendedor.RelatorioDeVendas());
+    }
 
    public void GerenteMenu(){
-       System.out.println("Nome/usuario: \n" +this.gerente.getUsuario() +
-                         "Email: \n" + "Tipo de conta: "+this.gerente.isAdministrador());
-       System.out.printf();
+       System.out.println("Nome/usuario: \n" + gerente.getUsuario() +
+                         "Email: \n" + "Tipo de conta: "+gerente.isAdministrador());
+       System.out.println("Relatorios de vendas \n"+ Gerente.GerarRelatorio()+"\n"+
+                             "Vendas realizadas: "+ Vendedor.RelatorioDeVendas());
    }
 
 
