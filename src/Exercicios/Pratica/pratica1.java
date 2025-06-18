0031a3aa368ea9c8db36d034722c3df34ec2c2be
S4Ktz <jaaow11@gmail.com>
2025-06-18 11:47:38 -0300

    ArrayList
    Praticando arrayList#1

---
+++ src/Exercicios/Pratica/pratica1.java
@@ -0,0 +1,25 @@
+package Exercicios.Pratica;
+
+import java.util.ArrayList;
+
+public class pratica1 {
+    public static void main(String[] args) {
+
+
+        ArrayList<String> teste = new ArrayList<String>();
+        teste.add("lalalala");
+        teste.add("teste");
+        teste.add("Lula");
+        teste.add("goku");
+        teste.add("dingling");
+        teste.add("ronaldinho");
+        teste.add("joão");
+
+        System.out.println("Imprimindo ArrayList");
+        for (int i = 0; i < teste.toArray().length; i++){
+            System.out.println(teste.get(i));
+
+
+        }
+    }
+}
