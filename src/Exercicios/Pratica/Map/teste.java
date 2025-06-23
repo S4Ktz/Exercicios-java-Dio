200044944de237ce6497602cace3cd15748e8e00
S4Ktz <jaaow11@gmail.com>
2025-06-23 16:12:04 -0300

    Update teste.java

--- src/Exercicios/Pratica/Map/teste.java
+++ src/Exercicios/Pratica/Map/teste.java
@@ -5,11 +5,15 @@ import java.util.Map;
 
 public class teste {
     public static void main(String[] args) {
-        Map<String,User> users = new HashMap<>();
-        users.put("Joao@lalalala.com",  new User("joao",22));
-        users.put("victor@victor.com",  new User("victor",18));
-        users.put("Maria@Maria.com",  new User("Maria",32));
-        System.out.println(users);
+        Map<String, User> users = new HashMap<>();
+        users.put("Joao@lalalala.com", new User("joao", 22));
+        users.put("victor@victor.com", new User("victor", 18));
+        users.put("Maria@Maria.com", new User("Maria", 32));
+//        System.out.println(users);
+//        System.out.println("===================");
+//        users.keySet().forEach(System.out::println);
+//        System.out.println("==================");
+//        users.values().forEach(System.out::println);
     }
 }
 
